eb4830329a29fb87aaaf534034ac92aa02aef3e4
Kelsei Kyser <kkelsei127@yahoo.com>
2023-02-04 11:44:55 -0500

    added inquirer prompts for adding employee, role and dept, fixed typo in schema

--- Develop/public/script.js
+++ Develop/public/script.js
@@ -1,16 +1,73 @@
 const inquirer = require('inquirer');
 
 // const directoryPrompts = [{
-//     type: 'input',
+//     type: 'list',
 //     message: 'What would you like to do?',
 //     choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add an employee', 'Update an employee role'],
 //     name: 'Directory'
 // }];
 
-function directory(){
+
+function addDept(){
     inquirer.prompt([
         {
             type: 'input',
+            message: 'What is the departments name?',
+            name: dept_name
+        }
+    ])
+}
+
+function addRole(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the role called?',
+            name: title
+        },
+        {
+            type: 'input',
+            message: 'What is the salary amount?',
+            name: salary
+        },
+        {
+            type: 'input',
+            message: "What is the department's ID?",
+            name: department_id
+        }
+    ])
+}
+
+function addEmployee(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the employees first name?',
+            name: first_name
+        },
+        {
+            type: 'input',
+            message: 'What is the employees last name?',
+            name: last_name
+        },
+        {
+            type: 'input',
+            message: "What is the employee's role ID?",
+            name: role_id
+        },
+        {
+            type: 'input',
+            message: "What is the employee's department ID?",
+            name: department_id
+        }
+    ])
+}
+
+
+function directory(){
+    inquirer.prompt([
+        {
+            type: 'list',
             message: 'What would you like to do?',
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add an employee', 'Update an employee role', 'Exit'],
             name: 'Directory' 
@@ -24,10 +81,13 @@ function directory(){
             //view all employees
         }else if(data.Directory === "Add a department"){
             //add a department
+            addDept();
         }else if(data.Directory === "Add an employee"){
             //add an employee
+            addRole();
         }else if(data.Directory === "Update an employee role"){
             //update employee role
+            addEmployee();
         }else{return}
     })
 }
